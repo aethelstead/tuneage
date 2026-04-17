311af6b86d171575310b9d77212af882587a8435
aethelstead <aethelstead@protonmail.com>
2026-04-17 21:57:13 +0100

    Single page example

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import rehypeExternalLinks from 'rehype-external-links';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://aethelstead.github.io',
-    base: '/tuneage'
+    base: '/tuneage',
+    markdown: {
+        rehypePlugins: [
+            [rehypeExternalLinks, { target: '_blank', rel: ['noopener', 'noreferrer'] }]
+        ]
+    }
 });
